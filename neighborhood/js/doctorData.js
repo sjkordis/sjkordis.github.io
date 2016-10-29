7902b9cebf73ffd0b1f9f46d7b97fa78cfdd3638
Susan <sjkordis@users.noreply.github.com>
2016-10-29 17:23:37 -0600

    Create doctorData.js

---
+++ neighborhood/js/doctorData.js
@@ -0,0 +1,31 @@
+{
+  "docs": [
+	{
+		"name": "Christopher Eriksen, MD",
+		"address:" {
+			"office": "1120 E Elizabeth St, Ste F101",
+			"lat": 40.57465,
+			"lon": -105.05661
+		},
+		"uid": "christopher-eriksen"
+	},
+	{
+		"name": "Lisa Butler, DO",
+		"visit_address": {
+			"office": "1212 E Elizabeth St",
+			"lat": 40.57472,
+			"lon": -105.05516
+		},
+		"uid": "lisa-butler-do"
+	},
+	{
+		"name": "Jennifer Hendrick, MD",
+		"visit_address": {
+			"office": "1024 S Lemay Ave",
+			"lat": 40.57176,
+			"lon": -105.05557
+		  },
+		"uid": "jennifer-hendrick"
+	 }
+  ]
+}
